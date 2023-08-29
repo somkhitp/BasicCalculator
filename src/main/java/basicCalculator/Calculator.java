d77956a015af16c6c88302210b80a1ab6353beb4
somkhitp <s.phosavath@gmail.com>
2023-08-29 14:54:54 -0500

    Updates

--- src/main/java/basicCalculator/Calculator.java
+++ src/main/java/basicCalculator/Calculator.java
@@ -1,4 +1,4 @@
-package calculator;
+package basicCalculator;
 
 import java.util.*;
 
--- src/test/java/basicCalculator/CalculatorTest.java
+++ src/test/java/basicCalculator/CalculatorTest.java
@@ -1,12 +1,11 @@
 package basicCalculator;
 
-import calculator.Calculator;
 import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.*;
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
