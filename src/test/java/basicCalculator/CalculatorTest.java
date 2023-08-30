c3d89a90c368a0c2a30b940bbc7af2a7f7b1c59f
somkhitp <s.phosavath@gmail.com>
2023-08-30 13:09:04 -0500

    Updates

--- src/test/java/basicCalculator/CalculatorTest.java
+++ src/test/java/basicCalculator/CalculatorTest.java
@@ -1,9 +1,14 @@
 package basicCalculator;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import java.io.*;
-import java.util.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
